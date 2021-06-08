6214582c51601035995d174b163af73af36ed405
Programmer SIMRS <programmer@rsubanjar.local>
2021-06-08 11:16:36 +0700

    Penambahan folder template

---
+++ application/views/Template/Footer.php
@@ -0,0 +1,84 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+      <div class="footer-top">
+          <div class="container">
+              <!-- <div class="row">
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+                <h3>RSUD Kota Banjar</h3>
+                <p>
+                    Jln. Rumah Sakit No.5 <br>
+                    Kelurahan Hegarsari<br><br>
+                    <strong>Telpon :</strong> 0265-741032<br>
+                    <strong>Email:</strong> rsu_kotabanjar@yahoo.co.id<br>
+                </p>
+            </div>
+
+            <div class="col-lg-2 col-md-6 footer-links">
+                <h4>Rekomendasi</h4>
+                <ul>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Tentang Kita</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Alur</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Dokter</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Kontak</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Kamu tau bulan?</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Jawaban nya: Ya, itu Kamu</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Pelayanan</h4>
+                <ul>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#">Cari Rekamedik</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Mendapatkan info terkini</h4>
+                <p>Wa Gateway kudu na mah</p>
+                <form action="" method="post">
+                    <input type="email" name="email"><input type="submit" value="Subscribe">
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div> -->
+
+              <div class="mr-md-auto text-center text-md-left">
+                  <div class="copyright text-center my-auto">
+                      <span>Copyright &copy; <strong> RSUD KOTA BANJAR </strong> <?= date('Y'); ?>. All Rights Reserved</span>
+                  </div>
+              </div>
+
+              <!-- <div class="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        </div> -->
+
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  </body>
+
+  </html>
---
+++ application/views/Template/Header.php
@@ -0,0 +1,26 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo mr-auto">
+            <marquee>RSUD Kota Banjar</marquee>
+        </h1>
+
+        <nav class="nav-menu d-none d-lg-block mr-auto">
+            <ul>
+                <li class="active"><a href="">Home</a></li>
+                <li><a href="#tentangkita">Tentang Kita</a></li>
+                <!-- <li><a href="#services">Services</a></li> -->
+                <li><a href="#alur">Alur</a></li>
+                <li><a href="#dokter">Dokter</a></li>
+                <li><a href="#contact">Kontak</a></li>
+                <li><a href="#lokasi">Lokasi</a></li>
+            </ul>
+        </nav>
+
+        <!-- .nav-menu -->
+
+        <a href="#appointment" class="appointment-btn scrollto">Daftar</a>
+
+    </div>
+</header><!-- End Header -->
