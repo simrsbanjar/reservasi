a5ff982752b2cc0b6b410871bf0586e7c3036cad
Programmer SIMRS <programmer@rsubanjar.local>
2021-06-16 10:15:10 +0700

    Penambahan script slider

--- application/views/Template/Footer.php
+++ application/views/Template/Footer.php
@@ -30,6 +30,10 @@
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
 
+<!-- slider -->
+<script src="assets/js/script.js"></script>
+<script src="assets/js/JQuery3.3.1.js"></script>
+<script src="assets/js/lightslider.js"></script>
 
 <script>
     $(document).ready(function() {
@@ -1187,6 +1191,21 @@
 
     $('#kecamatan').change(function() {
         GetKelurahan();
+    });
+
+
+    $(function() {
+        $('#WAButton').floatingWhatsApp({
+            phone: '1231231231', //WhatsApp Business phone number International format-
+            //Get it with Toky at https://toky.co/en/features/whatsapp.
+            headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+            popupMessage: 'Hello, how can we help you?', //Popup Message
+            showPopup: true, //Enables popup display
+            buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+            //headerColor: 'crimson', //Custom header color
+            //backgroundColor: 'crimson', //Custom background button color
+            position: "right"
+        });
     });
 </script>
 
