8c2a8fc4c40d1ba821cdaa91bb445b3ca7666613
Programmer SIMRS <programmer@rsubanjar.local>
2021-06-18 10:18:26 +0700

    penambahan tag head dan tag html

--- application/views/Cetak_noantrian.php
+++ application/views/Cetak_noantrian.php
@@ -1,90 +1,129 @@
-<!-- Bagian ini tag head digunakan untuk penampung tag 
-<title>, <style>, <meta>, <link>, <script>, and <base>. -->
-<?php $this->load->view('Template/Head'); ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
+    <title>Pendaftaran Online RSUD Kota Banjar</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-<div class="antrian">
-    <div class="container">
-        <div class="gambar">
-            <img src="assets/img/logo rsu.png" alt="logo rumah sakit">
+    <!-- Favicons -->
+    <link rel="icon" href="<?= base_url('assets/img/logo rsu.png'); ?>" type="image/png" sizes="16x16">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Medilab - v2.1.1
+  * Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <div class="antrian">
+        <div class="container">
+            <div class="gambar">
+                <img src="assets/img/logo rsu.png" alt="logo rumah sakit">
+                <p>
+                    Rumah Sakit Umum Daerah Kota Banjar
+                </p>
+            </div>
             <p>
-                Rumah Sakit Umum Daerah Kota Banjar
+                Jl. Rumah Sakit Umum No.5, Hegarsari, Kec. Pataruman, Kota Banjar, Jawa Barat 40293
             </p>
-        </div>
-        <p>
-            Jl. Rumah Sakit Umum No.5, Hegarsari, Kec. Pataruman, Kota Banjar, Jawa Barat 40293
-        </p>
-        <table>
+            <table>
 
-            <div>
-                <p style="margin-top: 10px;">Nomer Antrian Anda</p>
-                <div class="nomerantri">
-                    <div>
-                        <p>001</p>
+                <div>
+                    <p style="margin-top: 10px;">Nomer Antrian Anda</p>
+                    <div class="nomerantri">
+                        <div>
+                            <p>001</p>
+                        </div>
                     </div>
                 </div>
+            </table>
+            <div class="urutan row justify-content-center" style="padding-top: 10px;">
+                <div class="nopendaftaran">
+                    No. Pendaftaran :
+                </div>
+                <div class="nomer">
+                    <p>A001</p>
+                </div>
             </div>
-        </table>
-        <div class="urutan row justify-content-center" style="padding-top: 10px;">
-            <div class="nopendaftaran">
-                No. Pendaftaran :
+            <div class="urutan row justify-content-center">
+                <div class="politujuan">
+                    Poli Tujuan :
+                </div>
+                <div class="politujuan">
+                    <p>Poli Jantung</p>
+                </div>
             </div>
-            <div class="nomer">
-                <p>A001</p>
+            <div class="urutan row justify-content-center">
+                <div class="doktertugas">
+                    Dokter Tujuan :
+                </div>
+                <div class="doktertugas">
+                    <p>Dr. Asep</p>
+                </div>
             </div>
-        </div>
-        <div class="urutan row justify-content-center">
-            <div class="politujuan">
-                Poli Tujuan :
+            <div class="urutan row justify-content-center">
+                <div class="estimasi">
+                    Estimasi Dilayani :
+                </div>
+                <div class="estimasi">
+                    <p>17-06-2021 09.00</p>
+                </div>
             </div>
-            <div class="politujuan">
-                <p>Poli Jantung</p>
+            <div class="urutan row justify-content-center">
+                <div class="estimasi">
+                    No. Rekam Medik :
+                </div>
+                <div class="estimasi">
+                    <p>4319101</p>
+                </div>
             </div>
-        </div>
-        <div class="urutan row justify-content-center">
-            <div class="doktertugas">
-                Dokter Tujuan :
+            <div class="urutan row justify-content-center">
+                <div class="estimasi">
+                    Status Pasien :
+                </div>
+                <div class="estimasi">
+                    <p>Pasien Baru</p>
+                </div>
             </div>
-            <div class="doktertugas">
-                <p>Dr. Asep</p>
-            </div>
-        </div>
-        <div class="urutan row justify-content-center">
-            <div class="estimasi">
-                Estimasi Dilayani :
-            </div>
-            <div class="estimasi">
-                <p>17-06-2021 09.00</p>
-            </div>
-        </div>
-        <div class="urutan row justify-content-center">
-            <div class="estimasi">
-                No. Rekam Medik :
-            </div>
-            <div class="estimasi">
-                <p>4319101</p>
-            </div>
-        </div>
-        <div class="urutan row justify-content-center">
-            <div class="estimasi">
-                Status Pasien :
-            </div>
-            <div class="estimasi">
-                <p>Pasien Baru</p>
-            </div>
-        </div>
 
 
-        <div class="qrcode">
-            <img src="assets/img/qrcode.png" alt="logo qrcode">
-        </div>
+            <div class="qrcode">
+                <img src="assets/img/qrcode.png" alt="logo qrcode">
+            </div>
 
-        <div style="align-items: center;">
-            <p>Mohon datang <b>30 menit</b> sebelumnya </p>
-            <p>Jika terlambat antrian tidak berlaku lagi</p>
-            <p style="font-weight: bold;">!!! Bawalah tanda bukti ini untuk verifikasi di pendaftaran !!! </p>
+            <div style="align-items: center;">
+                <p>Mohon datang <b>30 menit</b> sebelumnya </p>
+                <p>Jika terlambat antrian tidak berlaku lagi</p>
+                <p style="font-weight: bold;">!!! Bawalah tanda bukti ini untuk verifikasi di pendaftaran !!! </p>
+            </div>
         </div>
+        <!-- tutup div container -->
     </div>
-    <!-- tutup div container -->
-</div>
+    <!-- tutup div class antrian -->
+
+</body>
+
+</html>
