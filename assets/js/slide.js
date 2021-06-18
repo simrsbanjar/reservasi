0efbf0afa8bd2a9bb27044e1db4cbc229c4750b3
Programmer SIMRS <programmer@rsubanjar.local>
2021-06-18 08:48:16 +0700

    Pernambahan js slide dibuat dengan file tinggap panggil

---
+++ assets/js/slide.js
@@ -0,0 +1,8 @@
+// file js untuk nge slide 
+    $(document).ready(function(){
+        $('.slidercuy').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        slidesToShow: 4
+        });
+    });
