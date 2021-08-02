a3e45e6d86002dc97ee550b01b00b5a7aa95ca49
Programmer SIMRS <programmer@rsubanjar.local>
2021-08-02 09:15:58 +0700

    Menghapus samesite=none di config.php pada ['cookie_path'] karena tidak support versi php 7.3 keatas

--- application/config/config.php
+++ application/config/config.php
@@ -402,7 +402,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 */
 $config['cookie_prefix']    = '';
 $config['cookie_domain']    = '';
-$config['cookie_path']      = '/; SameSite=None';
+$config['cookie_path']      = '/';
 $config['cookie_secure']    = TRUE;
 $config['cookie_httponly']     = FALSE;
 
