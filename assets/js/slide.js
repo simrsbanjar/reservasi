2953e26fb81f9bd6547d83125f6afed4bd1822a4
Programmer SIMRS <programmer@rsubanjar.local>
2021-09-29 11:21:22 +0700

    Perubahan js slide vendor slic menjadi 1 show per slide

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -4,14 +4,14 @@
         dots: true,
         autoplay: true,
         autoplaySpeed: 2500,
-        slidesToShow: 6,
+        slidesToShow: 1,
         slidesToScroll:1,
         responsive: [
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
               }
@@ -20,16 +20,16 @@
               breakpoint: 600,
               settings: {
                 dots: false,
-                slidesToShow: 3,
-                slidesToScroll: 3
+                slidesToShow: 1,
+                slidesToScroll: 1
               }
             },
             {
               breakpoint: 480,
               settings: {
                 dots:false,
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
               }
             }
             // You can unslick at a given breakpoint now by adding:
@@ -38,3 +38,43 @@
           ]
         });
     });
+
+  //   $(document).ready(function(){
+  //     $('.slidercuy').slick({
+  //     dots: true,
+  //     autoplay: true,
+  //     autoplaySpeed: 2500,
+  //     slidesToShow: 6,
+  //     slidesToScroll:1,
+  //     responsive: [
+  //         {
+  //           breakpoint: 1024,
+  //           settings: {
+  //             slidesToShow: 4,
+  //             slidesToScroll: 4,
+  //             infinite: true,
+  //             dots: true
+  //           }
+  //         },
+  //         {
+  //           breakpoint: 600,
+  //           settings: {
+  //             dots: false,
+  //             slidesToShow: 3,
+  //             slidesToScroll: 3
+  //           }
+  //         },
+  //         {
+  //           breakpoint: 480,
+  //           settings: {
+  //             dots:false,
+  //             slidesToShow: 2,
+  //             slidesToScroll: 2
+  //           }
+  //         }
+  //         // You can unslick at a given breakpoint now by adding:
+  //         // settings: "unslick"
+  //         // instead of a settings object
+  //       ]
+  //     });
+  // });
